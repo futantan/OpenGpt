bb374ef073de49e6497514c5dd5f4846442f897a
futantan <luckytantanfu@gmail.com>
2023-03-03 20:30:28 +0800

    refactor: 💡 use Absolute Imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
+import Github from '@/components/GitHub';
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { useRef, useState } from "react";
-import { Toaster, toast } from "react-hot-toast";
+import { toast, Toaster } from "react-hot-toast";
 import DropDown, { VibeType } from "../components/DropDown";
 import Footer from "../components/Footer";
-import Github from "../components/GitHub";
 import Header from "../components/Header";
 import LoadingDots from "../components/LoadingDots";
 
