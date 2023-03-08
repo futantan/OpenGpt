d33fb0195b9568d53049874415a65cbe2ca9d85b
futantan <luckytantanfu@gmail.com>
2023-03-08 08:45:59 +0800

    feat: 🎸 add layout

--- src/pages/app/[id].tsx
+++ src/pages/app/[id].tsx
@@ -1,11 +1,11 @@
 import Footer from '@/components/Footer'
+import Layout from '@/components/Layout'
 import LoadingDots from '@/components/LoadingDots'
 import { useGenerateResult } from '@/hooks/useGenerateResult'
 import { appRouter } from '@/server/api/root'
 import { prisma } from '@/server/db'
 import type { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 import Head from 'next/head'
-import Link from 'next/link'
 import { useRef, useState } from 'react'
 import { toast } from 'react-hot-toast'
 
@@ -72,75 +72,73 @@ const OpenGptApp = (
   }
 
   return (
-    <div className="mx-auto flex min-h-screen max-w-3xl flex-col items-center justify-center py-2">
-      <Head>
-        <title>{name}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Layout>
+      <div className="mx-auto flex max-w-3xl flex-col items-center justify-center py-2">
+        <Head>
+          <title>{name}</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className="mt-12 flex w-full flex-1 flex-col items-center justify-center px-4 text-center sm:mt-20">
-        <div className="mb-20 flex w-full">
-          <Link href="/">&#8592;首页</Link>
-        </div>
+        <main className="mt-12 flex w-full flex-1 flex-col items-center justify-center px-4 text-center sm:mt-20">
+          <h1 className="max-w-[708px] text-4xl font-bold text-slate-900 sm:text-6xl">
+            {name}
+          </h1>
 
-        <h1 className="max-w-[708px] text-4xl font-bold text-slate-900 sm:text-6xl">
-          {name}
-        </h1>
+          <p className="mt-6 text-lg leading-8 text-gray-600">{description}</p>
 
-        <p className="mt-6 text-lg leading-8 text-gray-600">{description}</p>
-
-        <div className="w-full max-w-xl">
-          {/* <div className="mt-10 flex items-center space-x-3">
+          <div className="w-full max-w-xl">
+            {/* <div className="mt-10 flex items-center space-x-3">
             <p className="text-left font-medium">{hint}</p>
           </div> */}
 
-          <textarea
-            value={userInput}
-            onChange={(e) => setUserInput(e.target.value)}
-            rows={4}
-            className="my-5 w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black"
-            placeholder={demoInput}
-          />
+            <textarea
+              value={userInput}
+              onChange={(e) => setUserInput(e.target.value)}
+              rows={4}
+              className="my-5 w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black"
+              placeholder={demoInput}
+            />
 
-          <button
-            className="mt-8 rounded-xl bg-black px-8 py-2 font-medium text-white hover:bg-black/80 sm:mt-10"
-            onClick={(e) => handleRun(e)}
-            disabled={loading}
-          >
-            {loading ? <LoadingDots color="white" style="large" /> : '运行'}
-          </button>
+            <button
+              className="mt-8 rounded-xl bg-black px-8 py-2 font-medium text-white hover:bg-black/80 sm:mt-10"
+              onClick={(e) => handleRun(e)}
+              disabled={loading}
+            >
+              {loading ? <LoadingDots color="white" style="large" /> : '运行'}
+            </button>
 
-          <div className="my-10 w-full space-y-10">
-            {generatedResults && (
-              <div className="flex flex-col gap-8">
-                <h2
-                  className="mx-auto text-3xl font-bold text-slate-900 sm:text-4xl"
-                  ref={resultRef}
-                >
-                  结果
-                </h2>
-                <div className="flex w-full flex-col items-center justify-center space-y-8">
-                  <div
-                    className="w-full cursor-copy rounded-xl border bg-white p-4 shadow-md transition hover:bg-gray-100"
-                    onClick={() => {
-                      navigator.clipboard.writeText(generatedResults)
-                      toast('Result copied to clipboard', {
-                        icon: '✂️',
-                      })
-                    }}
+            <div className="my-10 w-full space-y-10">
+              {generatedResults && (
+                <div className="flex flex-col gap-8">
+                  <h2
+                    className="mx-auto text-3xl font-bold text-slate-900 sm:text-4xl"
+                    ref={resultRef}
                   >
-                    <p className="whitespace-pre-line text-left">
-                      {generatedResults}
-                    </p>
+                    结果
+                  </h2>
+                  <div className="flex w-full flex-col items-center justify-center space-y-8">
+                    <div
+                      className="w-full cursor-copy rounded-xl border bg-white p-4 shadow-md transition hover:bg-gray-100"
+                      onClick={() => {
+                        navigator.clipboard.writeText(generatedResults)
+                        toast('Result copied to clipboard', {
+                          icon: '✂️',
+                        })
+                      }}
+                    >
+                      <p className="whitespace-pre-line text-left">
+                        {generatedResults}
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            )}
+              )}
+            </div>
           </div>
-        </div>
-      </main>
-      <Footer />
-    </div>
+        </main>
+        <Footer />
+      </div>
+    </Layout>
   )
 }
 
--- src/pages/app/new.tsx
+++ src/pages/app/new.tsx
@@ -1,5 +1,6 @@
 import Button from '@/components/Button'
 import { EmojiField } from '@/components/EmojiField'
+import Layout from '@/components/Layout'
 import { useGenerateResult } from '@/hooks/useGenerateResult'
 import { createAppSchema } from '@/server/api/schema'
 import { api, type RouterInputs } from '@/utils/api'
@@ -66,164 +67,167 @@ const NewApp = () => {
   }
 
   return (
-    <div className="bg-gray-200 pt-10">
-      <div className="mx-auto min-h-screen max-w-xl ">
-        <h1 className="py-10 text-center text-2xl font-semibold text-gray-900">
-          创建应用
-        </h1>
-        <form className=" space-y-6" onSubmit={handleSubmit(onSubmit)}>
-          <div className="bg-white px-4 py-5 shadow sm:rounded-lg sm:p-6">
-            <div className="mt-5 space-y-6 md:col-span-2 md:mt-0">
-              <div className="grid grid-cols-3 gap-6">
-                <div className="col-span-3 sm:col-span-2">
-                  <label className="block text-sm font-medium leading-6 text-gray-900">
-                    Icon
-                  </label>
+    <Layout>
+      <div className="bg-gray-200 pt-10">
+        <div className="mx-auto min-h-screen max-w-xl ">
+          <h1 className="py-10 text-center text-2xl font-semibold text-gray-900">
+            创建应用
+          </h1>
+          <form className=" space-y-6" onSubmit={handleSubmit(onSubmit)}>
+            <div className="bg-white px-4 py-5 shadow sm:rounded-lg sm:p-6">
+              <div className="mt-5 space-y-6 md:col-span-2 md:mt-0">
+                <div className="grid grid-cols-3 gap-6">
+                  <div className="col-span-3 sm:col-span-2">
+                    <label className="block text-sm font-medium leading-6 text-gray-900">
+                      Icon
+                    </label>
 
-                  <Controller
-                    name="icon"
-                    control={control}
-                    defaultValue="🤖"
-                    render={({ field }) => (
-                      <EmojiField
-                        value={field.value}
-                        onChange={(value) => field.onChange(value)}
+                    <Controller
+                      name="icon"
+                      control={control}
+                      defaultValue="🤖"
+                      render={({ field }) => (
+                        <EmojiField
+                          value={field.value}
+                          onChange={(value) => field.onChange(value)}
+                        />
+                      )}
+                    />
+
+                    <p className="mt-2 text-sm text-red-500">
+                      {errors.icon && errors.icon.message}
+                    </p>
+                    <p className="mt-2 text-sm text-gray-500">
+                      挑选一个 emoji 作为应用的图标吧！
+                    </p>
+                  </div>
+                </div>
+                <div className="grid grid-cols-3 gap-6">
+                  <div className="col-span-3 sm:col-span-2">
+                    <label className="block text-sm font-medium leading-6 text-gray-900">
+                      应用名称
+                    </label>
+                    <div className="mt-2 flex rounded-md shadow-sm">
+                      <input
+                        type="text"
+                        className="block w-full flex-1 rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        placeholder="智能翻译助手"
+                        {...register('name')}
                       />
-                    )}
-                  />
-
-                  <p className="mt-2 text-sm text-red-500">
-                    {errors.icon && errors.icon.message}
-                  </p>
-                  <p className="mt-2 text-sm text-gray-500">
-                    挑选一个 emoji 作为应用的图标吧！
-                  </p>
-                </div>
-              </div>
-              <div className="grid grid-cols-3 gap-6">
-                <div className="col-span-3 sm:col-span-2">
-                  <label className="block text-sm font-medium leading-6 text-gray-900">
-                    应用名称
-                  </label>
-                  <div className="mt-2 flex rounded-md shadow-sm">
-                    <input
-                      type="text"
-                      className="block w-full flex-1 rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      placeholder="智能翻译助手"
-                      {...register('name')}
-                    />
+                    </div>
+                    <p className="mt-2 text-sm text-red-500">
+                      {errors.name && errors.name.message}
+                    </p>
                   </div>
-                  <p className="mt-2 text-sm text-red-500">
-                    {errors.name && errors.name.message}
-                  </p>
                 </div>
-              </div>
-              <div className="grid grid-cols-3 gap-6">
-                <div className="col-span-3 sm:col-span-2">
-                  <label className="block text-sm font-medium leading-6 text-gray-900">
-                    应用描述
-                  </label>
-                  <div className="mt-2">
-                    <textarea
-                      rows={3}
-                      className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"
-                      placeholder="这款 App 可以将任意语言的内容，翻译成中文"
-                      defaultValue={''}
-                      {...register('description')}
-                    />
+                <div className="grid grid-cols-3 gap-6">
+                  <div className="col-span-3 sm:col-span-2">
+                    <label className="block text-sm font-medium leading-6 text-gray-900">
+                      应用描述
+                    </label>
+                    <div className="mt-2">
+                      <textarea
+                        rows={3}
+                        className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"
+                        placeholder="这款 App 可以将任意语言的内容，翻译成中文"
+                        defaultValue={''}
+                        {...register('description')}
+                      />
+                    </div>
+                    <p className="mt-2 text-sm text-red-500">
+                      {errors.description && errors.description.message}
+                    </p>
                   </div>
-                  <p className="mt-2 text-sm text-red-500">
-                    {errors.description && errors.description.message}
-                  </p>
                 </div>
-              </div>
-              <div className="grid grid-cols-3 gap-6">
-                <div className="col-span-3 sm:col-span-2">
-                  <label className="block text-sm font-medium leading-6 text-gray-900">
-                    指令
-                  </label>
-                  <div className="mt-2 flex rounded-md shadow-sm">
-                    <textarea
-                      rows={3}
-                      className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"
-                      placeholder="你是一个翻译官，无论接下来输入什么，你都要翻译成中文。内容是："
-                      defaultValue={''}
-                      {...register('prompt')}
-                    />
+                <div className="grid grid-cols-3 gap-6">
+                  <div className="col-span-3 sm:col-span-2">
+                    <label className="block text-sm font-medium leading-6 text-gray-900">
+                      指令
+                    </label>
+                    <div className="mt-2 flex rounded-md shadow-sm">
+                      <textarea
+                        rows={3}
+                        className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"
+                        placeholder="你是一个翻译官，无论接下来输入什么，你都要翻译成中文。内容是："
+                        defaultValue={''}
+                        {...register('prompt')}
+                      />
+                    </div>
+                    <p className="mt-2 text-sm text-red-500">
+                      {errors.prompt && errors.prompt.message}
+                    </p>
+                    <p className="mt-2 text-sm text-gray-500">
+                      指令需清晰易懂，明确且有逻辑。让 ChatGpt
+                      化身你的小帮手吧。
+                    </p>
                   </div>
-                  <p className="mt-2 text-sm text-red-500">
-                    {errors.prompt && errors.prompt.message}
-                  </p>
-                  <p className="mt-2 text-sm text-gray-500">
-                    指令需清晰易懂，明确且有逻辑。让 ChatGpt 化身你的小帮手吧。
-                  </p>
                 </div>
-              </div>
-              <div className="grid grid-cols-3 gap-6">
-                <div className="col-span-3 sm:col-span-2">
-                  <label className="block text-sm font-medium leading-6 text-gray-900">
-                    示例输入
-                  </label>
-                  <div className="mt-2 flex rounded-md shadow-sm">
-                    <input
-                      type="text"
-                      className="block w-full flex-1 rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      placeholder="I love you three thousand times."
-                      {...register('demoInput')}
-                    />
-                  </div>
-                  <p className="mt-2 text-sm text-red-500">
-                    {errors.demoInput && errors.demoInput.message}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="flex justify-end gap-3 px-4 sm:px-0">
-            <Button category="tertiary" onClick={() => router.push('/')}>
-              Cancel
-            </Button>
-
-            <Button
-              category="secondary"
-              onClick={handleTest}
-              loading={isTesting}
-            >
-              测试
-            </Button>
-
-            <Button type="submit" loading={isCreating}>
-              创建
-            </Button>
-          </div>
-
-          <div className="my-10 w-full space-y-10">
-            {generatedResults && (
-              <div className="flex flex-col gap-8">
-                <h2 className="mx-auto text-3xl font-bold text-slate-900 sm:text-4xl">
-                  结果
-                </h2>
-                <div className="flex w-full flex-col items-center justify-center space-y-8">
-                  <div
-                    className="w-full cursor-copy rounded-xl border bg-white p-4 shadow-md transition hover:bg-gray-100"
-                    onClick={() => {
-                      navigator.clipboard.writeText(generatedResults)
-                      toast('Result copied to clipboard', {
-                        icon: '✂️',
-                      })
-                    }}
-                  >
-                    <p className="whitespace-pre-line text-left">
-                      {generatedResults}
+                <div className="grid grid-cols-3 gap-6">
+                  <div className="col-span-3 sm:col-span-2">
+                    <label className="block text-sm font-medium leading-6 text-gray-900">
+                      示例输入
+                    </label>
+                    <div className="mt-2 flex rounded-md shadow-sm">
+                      <input
+                        type="text"
+                        className="block w-full flex-1 rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        placeholder="I love you three thousand times."
+                        {...register('demoInput')}
+                      />
+                    </div>
+                    <p className="mt-2 text-sm text-red-500">
+                      {errors.demoInput && errors.demoInput.message}
                     </p>
                   </div>
                 </div>
               </div>
-            )}
-          </div>
-        </form>
+            </div>
+            <div className="flex justify-end gap-3 px-4 sm:px-0">
+              <Button category="tertiary" onClick={() => router.push('/')}>
+                Cancel
+              </Button>
+
+              <Button
+                category="secondary"
+                onClick={handleTest}
+                loading={isTesting}
+              >
+                测试
+              </Button>
+
+              <Button type="submit" loading={isCreating}>
+                创建
+              </Button>
+            </div>
+
+            <div className="my-10 w-full space-y-10">
+              {generatedResults && (
+                <div className="flex flex-col gap-8">
+                  <h2 className="mx-auto text-3xl font-bold text-slate-900 sm:text-4xl">
+                    结果
+                  </h2>
+                  <div className="flex w-full flex-col items-center justify-center space-y-8">
+                    <div
+                      className="w-full cursor-copy rounded-xl border bg-white p-4 shadow-md transition hover:bg-gray-100"
+                      onClick={() => {
+                        navigator.clipboard.writeText(generatedResults)
+                        toast('Result copied to clipboard', {
+                          icon: '✂️',
+                        })
+                      }}
+                    >
+                      <p className="whitespace-pre-line text-left">
+                        {generatedResults}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </Layout>
   )
 }
 
