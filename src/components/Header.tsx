2d59f85caf1db43ede3228db784b03ae0946fc46
futantan <luckytantanfu@gmail.com>
2023-03-10 00:11:40 +0800

    fix: 🐛 styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -108,7 +108,7 @@ export function Header() {
       <Container>
         <nav className="relative z-50 flex justify-between">
           <div className="flex items-center md:gap-x-12">
-            <Link href="#" aria-label="Home">
+            <Link href="/" aria-label="Home">
               <Logo className="h-10 w-auto" />
             </Link>
             <div className="hidden md:flex md:gap-x-6">
