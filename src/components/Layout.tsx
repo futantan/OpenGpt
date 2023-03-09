6d1f7f5c3d3880e1e21675ca83d99e6ebfb39584
futantan <luckytantanfu@gmail.com>
2023-03-10 01:02:58 +0800

    fix: 🐛 layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,11 @@
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
-import { Hero } from '@/components/Hero'
 
 const Layout = (props: { children?: JSX.Element | JSX.Element[] }) => {
   return (
     <>
       <Header />
       <main>
-        <Hero />
         <div className="w-full bg-slate-50 pb-20">
           <div className="mx-auto max-w-7xl px-4 pt-10 sm:px-6 lg:px-8">
             {props.children}
