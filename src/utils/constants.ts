37db3434b82a06d5af5d9594cae0ebe9311f77d4
futantan <luckytantanfu@gmail.com>
2023-03-11 21:19:53 +0800

    feat: 🎸 update limit

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,7 +4,7 @@ export const SITE_DESC =
   '立即使用海量的 ChatGPT 应用，或在几秒钟内创建属于自己的应用。'
 // 'Access a vast library of pre-built ChatGPT applications or create your own in just seconds, with OpenGPT platform.'
 
-export const RATE_LIMIT_COUNT = 15
+export const RATE_LIMIT_COUNT = 5
 
 export const HOST_URL = isDev
   ? 'http://localhost:3000'
