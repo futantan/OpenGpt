81ba61993f91825c98ef4610782f4f0324766944
futantan <luckytantanfu@gmail.com>
2023-03-11 16:38:36 +0800

    feat: 🎸 show app number

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -99,7 +99,7 @@ const Home = (
                     onChange={handleChange}
                     onCompositionEnd={handleCompositionEnd}
                     className="block w-full rounded-lg border border-gray-300 bg-white p-4 pl-10 text-sm text-gray-900 focus:border-blue-600 focus:ring-blue-600 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-600 dark:focus:ring-blue-600"
-                    placeholder="Search..."
+                    placeholder={`Search ${apps.length} apps...`}
                   />
                 </div>
               </div>
