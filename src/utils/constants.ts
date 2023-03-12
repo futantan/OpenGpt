3b386a4822becb9b74c18581af554893524ec8aa
futantan <luckytantanfu@gmail.com>
2023-03-13 07:56:00 +0800

    feat: 🎸 update rate limit

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,4 +4,4 @@ export const SITE_DESC =
 // 'Access a vast library of pre-built ChatGPT applications or create your own in just seconds, with OpenGPT platform.'
 
 export const MAX_TOKENS = 500
-export const RATE_LIMIT_COUNT = 10
+export const RATE_LIMIT_COUNT = 5
