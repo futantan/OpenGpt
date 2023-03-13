42ebbb843020657b60acc515fa598598304e30be
river <laolei@forkway.cn>
2023-03-13 10:46:59 +0800

    feat: 🎸 change new_action_btn position

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
 import { SITE_DESC } from '@/utils/constants'
+import { HandPointer } from '@/components/HandPointer'
+import { PlusCircleIcon } from '@heroicons/react/24/outline'
 
 export function Hero() {
   return (
-    <Container className="pt-20 pb-16 text-center lg:pt-32">
+    <Container className="pt-20 pb-12 text-center lg:pt-32">
       <h1 className="font-display mx-auto max-w-4xl text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
         Create{' '}
         <span className="relative whitespace-nowrap text-blue-600">
@@ -26,9 +28,18 @@ export function Hero() {
         {SITE_DESC}
       </p>
       <div className="mt-10 flex justify-center gap-x-6">
-        <Button href="https://github.com/futantan/OpenGpt" target="_blank">
+        {/*   <Button href="https://github.com/futantan/OpenGpt" target="_blank" variant="solid" >
           Star on GitHub
-        </Button>
+        </Button>*/}
+        <div className="flex items-center justify-end gap-4 ">
+          <HandPointer />
+          <Button variant="solid" color="blue" href="/app/new">
+            <div className="flex items-center gap-2">
+              <PlusCircleIcon className="h-6 w-6"></PlusCircleIcon>
+              <span className="mr-0.5 whitespace-nowrap">创建应用</span>
+            </div>
+          </Button>
+        </div>
       </div>
     </Container>
   )
