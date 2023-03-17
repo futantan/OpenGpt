97216881da1aef3e02d2732610a34d61c29f7254
Tantan Fu <luckytantanfu@gmail.com>
2023-03-17 23:28:07 +0800

    feat: 🎸 seo (#70)
    * refactor: 💡 remove unused page
    
    * feat: 🎸 seo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,34 +1,9 @@
-import { SITE_DESC, SITE_TITLE } from '@/utils/constants'
 import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function MyDocument() {
-  const title = SITE_TITLE
-  const description = SITE_DESC
-
   return (
     <Html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.png" />
-        <meta name="description" content={description} />
-        <meta
-          property="og:site_name"
-          content="https://open-gpt-app.vercel.app/"
-        />
-        <meta property="og:description" content={description} />
-        <meta property="og:title" content={title} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
-        <meta
-          property="og:image"
-          content="https://open-gpt-app.vercel.app/og-image.png"
-        />
-        <meta
-          name="twitter:image"
-          content="https://open-gpt-app.vercel.app/og-image.png"
-        />
-      </Head>
-
+      <Head />
       <body>
         <Main />
         <NextScript />
