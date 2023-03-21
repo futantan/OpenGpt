e8e2ca630cdc6714d82db359aa392a0a9e1d7d8e
futantan <luckytantanfu@gmail.com>
2023-03-21 23:39:39 +0800

    feat: 🎸 csr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 import AppList from '@/components/AppList'
+import AppListLoading from '@/components/AppListLoading'
 import { Button } from '@/components/Button'
 import { CallToAction } from '@/components/CallToAction'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Hero } from '@/components/Hero'
 import { SearchInput } from '@/components/SearchInput'
-import { appRouter } from '@/server/api/root'
-import { prisma } from '@/server/db'
-import type { GetStaticProps, InferGetServerSidePropsType } from 'next'
+import { api } from '@/utils/api'
+import type { GetStaticProps } from 'next'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import * as R from 'ramda'
@@ -19,38 +19,64 @@ type App = {
   description: string
   icon: string
 }
-type PageProps = { apps: App[] }
+type PageProps = {}
 
 export const getStaticProps: GetStaticProps<PageProps> = async ({ locale }) => {
-  const caller = appRouter.createCaller({ prisma, session: null })
-  const apps = await caller.app.getAll()
   return {
     props: {
-      apps,
       ...(await serverSideTranslations(locale!, ['common'])),
     },
-    // TODO:
+    // TODO: disabled because of i18n, and switched to CSR
     // revalidate: 120, // In seconds
   }
 }
 
-const Home = (props: InferGetServerSidePropsType<typeof getStaticProps>) => {
-  const { apps } = props
+const Home = () => {
   const [searchValue, setSearchValue] = useState('')
   const [sizeToShow, setSizeToShow] = useState(100)
   // @ts-ignore
   const { t } = useTranslation('common')
 
-  const list = searchValue
-    ? apps.filter(
-        (app) =>
-          app.name.includes(searchValue) ||
-          app.description.includes(searchValue)
-      )
-    : apps
+  const { isLoading, data: apps } = api.app.getAll.useQuery()
+
+  const list = (
+    searchValue
+      ? apps!.filter(
+          (app) =>
+            app.name.includes(searchValue) ||
+            app.description.includes(searchValue)
+        )
+      : apps
+  ) as App[]
 
   const handleShowMore = () => {
     setSizeToShow(sizeToShow + 100)
+  }
+
+  if (isLoading) {
+    return (
+      <>
+        <Header />
+        <main>
+          <Hero />
+          <div className="w-full bg-slate-50 pb-20 pt-10">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+              <div className="mb-10 grid grid-cols-1 items-center justify-between pt-10 sm:grid-cols-3 sm:pt-0 ">
+                <div />
+                <SearchInput
+                  setSearchValue={setSearchValue}
+                  placeholder={'Search apps...'}
+                />
+                <div />
+              </div>
+              <AppListLoading />
+            </div>
+          </div>
+          <CallToAction />
+        </main>
+        <Footer />
+      </>
+    )
   }
 
   return (
@@ -64,7 +90,7 @@ const Home = (props: InferGetServerSidePropsType<typeof getStaticProps>) => {
               <div />
               <SearchInput
                 setSearchValue={setSearchValue}
-                placeholder={`Search ${apps.length} apps...`}
+                placeholder={`Search ${apps!.length} apps...`}
               />
               <div />
             </div>
