242d2f3f2e2ce8440d4ac15e639bf205e96ba336
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 00:29:02 +0600

    improve

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,16 @@
-module.exports = {
-  i18n: {
-    defaultLocale: 'zh',
-    locales: ['zh', 'en', 'zh-Hant'],
-  },
-}
+// settings.js
+
+const i18nSettings = {
+  defaultLocale: 'zh',
+  locales: ['zh', 'en', 'zh-Hant'],
+};
+
+module.exports = i18nSettings;
+
+
+
+// someOtherFile.js
+
+const i18nSettings = require('./settings');
+
+// usage of i18nSettings in the code
