34f3573e5d4384f20155c72ed2d9e45513c06048
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 00:29:01 +0600

    comment

--- src/components/EmojiField.tsx
+++ src/components/EmojiField.tsx
@@ -1,6 +1,9 @@
-import { Popover } from '@headlessui/react'
-import EmojiPicker from 'emoji-picker-react'
+import { Popover } from '@headlessui/react' // Importing Popover component from Headless UI
+import EmojiPicker from 'emoji-picker-react' // Importing EmojiPicker component from emoji-picker-react
 
+// EmojiField component receives two props: value and onChange
+// value: string - the current emoji value
+// onChange: (v: string) => void - a callback function to update the emoji value
 export const EmojiField = ({
   value,
   onChange,
@@ -9,9 +12,12 @@ export const EmojiField = ({
   onChange: (v: string) => void
 }) => (
   <>
+    {/* Render a Popover component from Headless UI */}
     <Popover className="relative">
+      {/* Render Popover.Button as the trigger for the Popover component */}
       <Popover.Button>
         <span className="inline-flex rounded-lg bg-indigo-50 p-3 ring-4 ring-white">
+          {/* Display the current emoji value inside a div */}
           <div
             className="flex h-6 w-6 items-center justify-center"
             aria-hidden="true"
@@ -21,9 +27,12 @@ export const EmojiField = ({
         </span>
       </Popover.Button>
 
+      {/* Render Popover.Panel to contain the EmojiPicker component */}
       <Popover.Panel className="absolute z-10">
+        {/* Render EmojiPicker component and attach an onEmojiClick event handler */}
         <EmojiPicker onEmojiClick={({ emoji }) => onChange(emoji)} />
       </Popover.Panel>
     </Popover>
   </>
 )
+
